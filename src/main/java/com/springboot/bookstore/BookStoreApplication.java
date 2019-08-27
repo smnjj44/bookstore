4d1561860d0a185025314d5c996a5fb7fcb59c40
NameForSimon <565587783@qq.com>
2019-08-27 17:59:49 +0800

    增加日志方法

--- src/main/java/com/springboot/bookstore/BookStoreApplication.java
+++ src/main/java/com/springboot/bookstore/BookStoreApplication.java
@@ -3,11 +3,17 @@ package com.springboot.bookstore;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @MapperScan("com.springboot.bookstore.dao")//mapper和启动类不在同一目录下就要开启启动时对mapper的包扫描
-public class BookStoreApplication {
+public class BookStoreApplication extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 		SpringApplication.run(BookStoreApplication.class, args);
+	}
+	@Override//为了打包springboot项目
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(this.getClass());
 	}
 }
