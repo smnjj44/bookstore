2e7b53f1f324402ec5a6b3a5ce80c9361fbf1fdb
NameForSimon <565587783@qq.com>
2019-10-24 18:20:49 +0800

    增加日志方法

--- src/main/java/com/springboot/bookstore/bean/Manager.java
+++ src/main/java/com/springboot/bookstore/bean/Manager.java
@@ -1,66 +1,86 @@
 package com.springboot.bookstore.bean;
 
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
 public class Manager implements UserDetails {
-private int id;
-private String name;
-private String password;
-//权限角色字段
-private List<GrantedAuthority> authorities;
-public int getId() {
-	return id;
-}
-public void setId(int id) {
-	this.id = id;
-}
-public String getName() {
-	return name;
-}
-public void setName(String name) {
-	this.name = name;
-}
+    private int id;
+    private String name;
+    private String password;
+    //权限角色字段
+    private String auth;
 
-	@Override
-	public Collection<? extends GrantedAuthority> getAuthorities() {
-		return authorities;
-	}
-	@Override
-	public String getPassword() {
-	return password;
-}
 
-	@Override
-	public String getUsername() {
-		return name;
-	}
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        String[] authorities = auth.split(",");
+        List<GrantedAuthority> authorityList = new ArrayList<>();
+        for (String auth : authorities) {
+            authorityList.add(new SimpleGrantedAuthority(auth));
+        }
+        return authorityList;
+    }
 
-	@Override
-	public boolean isAccountNonExpired() {
-		return true;
-	}
+    @Override
+    public String getPassword() {
+        return password;
+    }
 
-	@Override
-	public boolean isAccountNonLocked() {
-		return true;
-	}
+    @Override
+    public String getUsername() {
+        return name;
+    }
 
-	@Override
-	public boolean isCredentialsNonExpired() {
-		return true;
-	}
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
 
-	@Override
-	public boolean isEnabled() {
-		return true;
-	}
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
 
-	public void setPassword(String password) {
-	this.password = password;
-}
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
 
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAuthority(String auth) {
+        this.auth = auth;
+    }
+
+    public String getAuthority() {
+        return auth;
+    }
 }
